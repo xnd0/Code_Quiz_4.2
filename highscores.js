47f8a327d6851280140a03c94eb99669303524d4
xndcmd <xndcmd@gmail.com>
2022-06-04 18:01:33 -0700

    got highscores to display something from localstorage

--- highscores.js
+++ highscores.js
@@ -4,5 +4,19 @@
 // -------------------------------------- //
 
 
+function displayScores() {
+    let scores = JSON.parse(localStorage.getItem("highScoreList"));
+
+    for (let index = 0; index < scores.length; index++) {
 
 
+        const scoreElement = document.createElement("li");
+        scoreElement.textContent = scores
+        scoreList.appendChild(scoreElement); 
+        
+    }
+}
+
+
+
+displayScores();
